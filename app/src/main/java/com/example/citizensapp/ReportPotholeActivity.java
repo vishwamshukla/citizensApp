13587dac04af0cf92889599d76b9b030543824de
shantanu354 <kanhapatildurg@gmail.com>
2020-07-20 02:19:36 +0530

    add report pothole activity

--- app/src/main/java/com/example/citizensapp/ReportPotholeActivity.java
+++ app/src/main/java/com/example/citizensapp/ReportPotholeActivity.java
@@ -3,6 +3,9 @@ package com.example.citizensapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ReportPotholeActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class ReportPotholeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_pothole);
+
+        TextView close_button = findViewById(R.id.close_settings_btn);
+        close_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
