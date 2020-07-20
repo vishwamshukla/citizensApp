272295c5b028d47c6883216c4133f69edbf0dc73
shantanu354 <kanhapatildurg@gmail.com>
2020-07-21 01:07:43 +0530

    add activity_report_pothole.xml, liked front-end with ReportPotholeActivity.java

--- app/src/main/java/com/example/citizensapp/ReportPotholeActivity.java
+++ app/src/main/java/com/example/citizensapp/ReportPotholeActivity.java
@@ -1,7 +1,9 @@
 package com.example.citizensapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,11 +16,33 @@ public class ReportPotholeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_pothole);
 
-        TextView close_button = findViewById(R.id.close_settings_btn);
-        close_button.setOnClickListener(new View.OnClickListener() {
+        Button open_camera = findViewById(R.id.pothole_camera_button);
+        open_camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                //TODO: Write code to open camera for image upload
+            }
+        });
+
+        Button upload_file = findViewById(R.id.pothole_upload_file);
+        upload_file.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final CharSequence[] items ={"Upload Image","Upload Video"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(ReportPotholeActivity.this);
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (items[i].equals("Upload Image")){
+                            //TODO: Write code for uploading Image
+                        }
+
+                        else if (items[i].equals("Upload Video")){
+                            //TODO: Write code for uploading Video
+                        }
+                    }
+                });
+                builder.show();
             }
         });
 
