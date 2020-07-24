bbe2639b01b3468cbf1449225d75bb891e99d06c
shantanu354 <kanhapatildurg@gmail.com>
2020-07-25 04:47:08 +0530

    Change app name

--- app/src/main/java/com/example/citizensapp/SplashActivity.java
+++ app/src/main/java/com/example/citizensapp/SplashActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private static int SPLASH_LENGTH = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +18,14 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         mAuth = FirebaseAuth.getInstance();
 
-        startActivity(new Intent(this, LoginActivity.class));
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, SPLASH_LENGTH);
+
     }
 
     //    @Override
