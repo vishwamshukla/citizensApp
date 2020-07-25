b744b140a9c1b5866a5654cc41cd01baba019bc0
Vishwam Shukla <shuklavishwam21@gmail.com>
2020-07-25 14:55:08 +0530

    new Splash Screen

---
+++ app/src/main/java/com/example/citizensapp/SplashScreen2.java
@@ -0,0 +1,47 @@
+package com.example.citizensapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen2 extends AppCompatActivity {
+    private static int SPLASH_SCREEN = 5000;
+
+    Animation topanim, bottomanim;
+    ImageView image;
+    TextView logo, slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen2);
+
+        topanim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomanim = AnimationUtils.loadAnimation(this,R.anim.bottom_navigation);
+
+        image = findViewById(R.id.imagesplash);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView2);
+
+        image.setAnimation(topanim);
+        logo.setAnimation(bottomanim);
+        slogan.setAnimation(bottomanim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen2.this, LoginActivity.class));
+                finish();
+            }
+        }, SPLASH_SCREEN);
+
+    }
+}
