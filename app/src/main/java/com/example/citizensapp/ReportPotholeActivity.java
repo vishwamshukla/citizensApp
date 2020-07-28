d36e5802a685e621052e010963263a8345bd7e3f
Vishwam Shukla <shuklavishwam21@gmail.com>
2020-07-28 15:42:22 +0530

    speech to text done

--- app/src/main/java/com/example/citizensapp/ReportPotholeActivity.java
+++ app/src/main/java/com/example/citizensapp/ReportPotholeActivity.java
@@ -13,17 +13,25 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Message;
 import android.provider.MediaStore;
+import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.MediaController;
@@ -56,12 +64,63 @@ import com.squareup.picasso.Picasso;
 import java.io.File;
 import java.io.IOException;
 
+import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+import com.ibm.cloud.sdk.core.http.HttpMediaType;
+import com.ibm.cloud.sdk.core.http.Response;
+import com.ibm.cloud.sdk.core.http.ServiceCall;
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.watson.assistant.v2.model.DialogNodeOutputOptionsElement;
+import com.ibm.watson.assistant.v2.model.RuntimeResponseGeneric;
+import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneHelper;
+import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneInputStream;
+import com.ibm.watson.developer_cloud.android.library.audio.StreamPlayer;
+import com.ibm.watson.developer_cloud.android.library.audio.utils.ContentType;
+import com.ibm.watson.assistant.v2.Assistant;
+import com.ibm.watson.assistant.v2.model.CreateSessionOptions;
+import com.ibm.watson.assistant.v2.model.MessageInput;
+import com.ibm.watson.assistant.v2.model.MessageOptions;
+import com.ibm.watson.assistant.v2.model.MessageResponse;
+import com.ibm.watson.assistant.v2.model.SessionResponse;
+import com.ibm.watson.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.speech_to_text.v1.model.SpeechRecognitionResults;
+import com.ibm.watson.speech_to_text.v1.websocket.BaseRecognizeCallback;
+import com.ibm.watson.text_to_speech.v1.TextToSpeech;
+import com.ibm.watson.text_to_speech.v1.model.SynthesizeOptions;
 
 public class ReportPotholeActivity extends AppCompatActivity {
+//    private RecyclerView recyclerView;
+//    private ChatAdapter mAdapter;
+    private ArrayList messageArrayList;
+    private EditText inputMessage;
+    private ImageButton btnSend;
+    private ImageButton btnRecord;
+    StreamPlayer streamPlayer = new StreamPlayer();
+    private boolean initialRequest;
+    private boolean permissionToRecordAccepted = false;
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private static String TAG = "MainActivity";
+    private static final int RECORD_REQUEST_CODE = 101;
+    private boolean listening = false;
+    private MicrophoneInputStream capture;
+    private Context mContext;
+    private MicrophoneHelper microphoneHelper;
+
+    private Assistant watsonAssistant;
+    private Response<SessionResponse> watsonAssistantSession;
+    private SpeechToText speechService;
+    private TextToSpeech textToSpeech;
+
+
+
     private static final int PICK_VIDEO_REQUEST=1;
     private static final int PICK_IMAGE_REQUEST = 1;
     public static final int CAMERA_PERM_CODE = 101;
@@ -92,21 +151,58 @@ public class ReportPotholeActivity extends AppCompatActivity {
 
 
 
+    private void createServices() {
+//        watsonAssistant = new Assistant("2019-02-28", new IamAuthenticator(mContext.getString(R.string.assistant_apikey)));
+//        watsonAssistant.setServiceUrl(mContext.getString(R.string.assistant_url));
+
+        textToSpeech = new TextToSpeech(new IamAuthenticator((mContext.getString(R.string.TTS_apikey))));
+        textToSpeech.setServiceUrl(mContext.getString(R.string.TTS_url));
+
+        speechService = new SpeechToText(new IamAuthenticator(mContext.getString(R.string.STT_apikey)));
+        speechService.setServiceUrl(mContext.getString(R.string.STT_url));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_pothole);
 
+        inputMessage = findViewById(R.id.potholes_comments_textview);
+        btnRecord = findViewById(R.id.btn_record);
+//        String customFont = "Montserrat-Regular.ttf";
+//        Typeface typeface = Typeface.createFromAsset(getAssets(), customFont);
+//        inputMessage.setTypeface(typeface);
 
+        microphoneHelper = new MicrophoneHelper(this);
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO);
 
-        videoUploadnew = findViewById(R.id.video_view_button);
-        videoUploadnew.setOnClickListener(new View.OnClickListener() {
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission to record denied");
+            makeRequest();
+        } else {
+            Log.i(TAG, "Permission to record was already granted");
+        }
+
+        btnRecord.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                uploadFile1();
-                startActivity(new Intent(ReportPotholeActivity.this, UploadVideoActivity.class));
+                getSpeechInput(v);
             }
         });
+
+        //createServices();
+
+
+
+//        videoUploadnew = findViewById(R.id.video_view_button);
+//        videoUploadnew.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                uploadFile1();
+//                startActivity(new Intent(ReportPotholeActivity.this, UploadVideoActivity.class));
+//            }
+//        });
 
 
 
@@ -118,7 +214,7 @@ public class ReportPotholeActivity extends AppCompatActivity {
         mProgressBar = findViewById(R.id.progress_bar);
         mEditTextPothole_Type = findViewById(R.id.pothole_type_textView);
         mEditTextAddress = findViewById(R.id.pothole_address_textView);
-        mEditTextComments = findViewById(R.id.potholes_comments_textview);
+       // mEditTextComments = findViewById(R.id.potholes_comments_textview);
         mEditTextDimensions = findViewById(R.id.pothole_dimension_textview);
         mEditTextLandmark = findViewById(R.id.pothole_landmark_textview);
         mAuth = FirebaseAuth.getInstance();
@@ -244,13 +340,26 @@ public class ReportPotholeActivity extends AppCompatActivity {
                 && data != null && data.getData() != null){
             mImageUri = data.getData();
             Picasso.get().load(mImageUri).into(mImageView);
-
-        }
-        else if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK){
+        }     else if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK){
             File f = new File(currentPhotoPath);
             mImageView.setImageURI(Uri.fromFile(f));
             mImageUri = Uri.fromFile(f);
         }
+
+        switch (requestCode){
+
+            case 10:
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    inputMessage.setText(result.get(0));
+                }
+                break;
+        }
+
+
+        }
+
+
         /*if (requestCode == PICK_VIDEO_REQUEST && resultCode == RESULT_OK
                 && data != null && data.getData() != null) {
 
@@ -258,7 +367,6 @@ public class ReportPotholeActivity extends AppCompatActivity {
 
             mVideoView.setVideoURI(mVideoUri);
         }*/
-    }
 
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -329,7 +437,7 @@ public class ReportPotholeActivity extends AppCompatActivity {
                                     String mAddress = mEditTextAddress.getEditText().getText().toString();
                                     String mLandmark = mEditTextLandmark.getEditText().getText().toString();
                                     String mDimension = mEditTextDimensions.getEditText().getText().toString().trim();
-                                    String mComment = mEditTextComments.getEditText().getText().toString();
+                                    String mComment = inputMessage.getText().toString();
                                     String mDate = dateFormat.format(date).toString();
                                     String mDateFull = datefull.format(date1).toString();
                                     String mTime = timeformat.format(time).toString();
@@ -585,6 +693,137 @@ public class ReportPotholeActivity extends AppCompatActivity {
         }
     }
 
+    protected void makeRequest() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.RECORD_AUDIO},
+                MicrophoneHelper.REQUEST_PERMISSION);
+    }
+    //Record a message via Watson Speech to Text
+    private void recordMessage() {
+        if (listening != true) {
+            capture = microphoneHelper.getInputStream(true);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        speechService.recognizeUsingWebSocket(getRecognizeOptions(capture), new MicrophoneRecognizeDelegate());
+                    } catch (Exception e) {
+                        showError(e);
+                    }
+                }
+            }).start();
+            listening = true;
+            Toast.makeText(ReportPotholeActivity.this, "Listening....Click to Stop", Toast.LENGTH_LONG).show();
 
+        } else {
+            try {
+                microphoneHelper.closeInputStream();
+                listening = false;
+                Toast.makeText(ReportPotholeActivity.this, "Stopped Listening....Click to Start", Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-}
+        }
+    }
+
+    /**
+     * Check Internet Connection
+     *
+     * @return
+     */
+    private boolean checkInternetConnection() {
+        // get Connectivity Manager object to check connection
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+        // Check for network connections
+        if (isConnected) {
+            return true;
+        } else {
+            Toast.makeText(this, " No Internet Connection available ", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+    }
+
+    //Private Methods - Speech to Text
+    private RecognizeOptions getRecognizeOptions(InputStream audio) {
+        return new RecognizeOptions.Builder()
+                .audio(audio)
+                .contentType(ContentType.OPUS.toString())
+                .model("en-US_BroadbandModel")
+                .interimResults(true)
+                .inactivityTimeout(2000)
+                .build();
+    }
+
+    private void showMicText(final String text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                inputMessage.setText(text);
+            }
+        });
+    }
+
+    private void enableMicButton() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                btnRecord.setEnabled(true);
+            }
+        });
+    }
+
+    private void showError(final Exception e) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(ReportPotholeActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        });
+    }
+
+    //Watson Speech to Text Methods.
+    private class MicrophoneRecognizeDelegate extends BaseRecognizeCallback {
+        @Override
+        public void onTranscription(SpeechRecognitionResults speechResults) {
+            if (speechResults.getResults() != null && !speechResults.getResults().isEmpty()) {
+                String text = speechResults.getResults().get(0).getAlternatives().get(0).getTranscript();
+                showMicText(text);
+            }
+        }
+
+        @Override
+        public void onError(Exception e) {
+            showError(e);
+            enableMicButton();
+        }
+
+        @Override
+        public void onDisconnected() {
+            enableMicButton();
+        }
+
+    }
+
+    public void getSpeechInput(View view) {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    }
