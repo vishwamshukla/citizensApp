a1e3f7d2d77f7c4cfd08b6c7d608ccc8b21ac601
shantanu354 <kanhapatildurg@gmail.com>
2020-07-30 19:39:57 +0530

    Add chats_menu in ChatsActivity

--- app/src/main/java/com/example/citizensapp/ChatsActivity.java
+++ app/src/main/java/com/example/citizensapp/ChatsActivity.java
@@ -1,9 +1,15 @@
 package com.example.citizensapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -15,6 +21,14 @@ public class ChatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats);
+
+        Toolbar my_toolbar = (Toolbar) findViewById(R.id.action_bar);
+        my_toolbar.setTitle("");
+        setSupportActionBar(my_toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         adapter = new TabAdapter(getSupportFragmentManager());
@@ -23,5 +37,33 @@ public class ChatsActivity extends AppCompatActivity {
         adapter.addFragment(new ConnectionsFragment(), "Connections");
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.chats_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.civilAgencies_list:
+                startActivity(new Intent(ChatsActivity.this, CivilAgenciesActivity.class));
+                return true;
+            case R.id.electedRepresentatives_list:
+                startActivity(new Intent(ChatsActivity.this, ElectedRepresentativesActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ app/src/main/java/com/example/citizensapp/ElectedRepresentativesActivity.java
@@ -0,0 +1,36 @@
+package com.example.citizensapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class ElectedRepresentativesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elected_representatives);
+
+        Toolbar my_toolbar = (Toolbar) findViewById(R.id.action_bar);
+        my_toolbar.setTitle("");
+        setSupportActionBar(my_toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.chats_menu, menu);
+        return true;
+    }
+}
